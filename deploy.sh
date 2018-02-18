3dfe807a1b2b33b6ed22f1b882b939bbaaccf875
Felix Descoteaux <flx.descoteaux@gmail.com>
2018-02-18 17:12:31 -0500

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+hugo
+modified_files=$(git diff-tree --no-commit-id --name-only -r HEAD | grep "^public/" | sed 's:^public/::')
+
+for f in $modified_files
+do
+    printf "\n\e[35mUploading \e[32m$f...\e[39m"
+    printf "\e[34m"
+    curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://ftp.software-rambles.com/$f
+    printf "\e[39m"
+done
